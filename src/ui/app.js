228b78adb0549e16b6481e2a4113e29296e4cde1
elliebeph <84377742+elliebeph@users.noreply.github.com>
2021-05-21 15:47:45 +0100

    Remove and replace React boilerplate

---
+++ src/ui/app.js
@@ -0,0 +1,15 @@
+import '../css/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          ALBW Tracker
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
