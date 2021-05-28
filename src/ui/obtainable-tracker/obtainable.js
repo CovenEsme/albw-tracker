aaacd7d96bda1b5924021bc564b9721773a40d5a
elliebeph <84377742+elliebeph@users.noreply.github.com>
2021-05-28 15:07:46 +0100

    Display an alert when an Obtainable is clicked

--- src/ui/obtainable-tracker/obtainable.js
+++ src/ui/obtainable-tracker/obtainable.js
@@ -4,7 +4,8 @@ class Obtainable extends React.Component {
   render() {
     return (
       <div className="obtainable">
-        <img src={this.props.image} alt={this.props.obtainableName}/>
+        <img src={this.props.image} alt={this.props.obtainableName}
+             onClick={() => alert('Click')}/>
       </div>
     )
   }
