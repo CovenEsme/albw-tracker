518748b86e818ca49ccc8fd624a41a5204dcfec1
elliebeph <84377742+elliebeph@users.noreply.github.com>
2021-08-13 12:12:03 +0100

    Add first App tests (proof-of-concept)

---
+++ src/ui/app.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import App from './app';
+import Images from '../services/images';
+
+describe('App', () => {
+  test('Renders App with loading spinner while initializing', async () => {
+    render(<App />);
+
+    expect(screen.queryByLabelText('audio-loading')).toBeInTheDocument();
+    expect(screen.queryByAltText('Gear overlay')).toBeNull();
+    expect(screen.queryByAltText('Items overlay')).toBeNull();
+    expect(screen.queryByAltText('Label')).toBeNull();
+  });
+
+  test('Renders App with tracker after initializing', async () => {
+    render(<App />);
+
+    expect(screen.queryByLabelText('audio-loading')).toBeInTheDocument();
+
+    expect(await screen.findByAltText('Gear overlay')).toBeInTheDocument();
+    expect(screen.queryByAltText('Items overlay')).toBeInTheDocument();
+    expect(screen.queryByAltText('Label')).toBeInTheDocument();
+  });
+});
