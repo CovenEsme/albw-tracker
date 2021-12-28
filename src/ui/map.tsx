081316f9ef60e42194c258c7aa299ca1c2dafa16
elliebeph <84377742+elliebeph@users.noreply.github.com>
2021-12-28 22:53:24 +0000

    Add initial failing Map tests

---
+++ src/ui/map.tsx
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import React from "react";
+
+// import Images from "../services/images";
+
+class Map extends React.Component {
+
+  render(): React.ReactNode {
+    return (
+      <div className="map">
+      </div>
+    );
+  }
+}
+
+export default Map;
