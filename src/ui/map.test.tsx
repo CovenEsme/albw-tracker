049cf19f189223b81dff54613b393b36f34ac27a
CovenEsme <84377742+elliebeph@users.noreply.github.com>
2022-05-05 22:13:08 +0100

    Add initial failing Map tests

---
+++ src/ui/map.test.tsx
@@ -0,0 +1,63 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Map from "./map";
+
+describe("Map", () => {
+  const mapHyruleAltText       = "Map of Hyrule";
+  const mapLoruleAltText       = "Map of Lorule";
+  const changeMapButtonAltText = "Change map button";
+
+  test("Renders Map with default Hyrule image", () => {
+    render(<Map />);
+
+    expect(screen.getByAltText(mapHyruleAltText)).toBeInTheDocument();
+  });
+
+  test("Renders Map with map change button", () => {
+    render(<Map />);
+
+    expect(screen.getByAltText(changeMapButtonAltText)).toBeInTheDocument();
+  });
+
+  test("Renders Lorule Map after the change map button is clicked", () => {
+    render(<Map />);
+
+    const mapOfHyrule = screen.getByAltText(mapHyruleAltText);
+    const changeMapButton = screen.getByAltText(changeMapButtonAltText);
+
+    expect(mapOfHyrule).toBeInTheDocument();
+    expect(changeMapButton).toBeInTheDocument();
+
+    fireEvent.click(changeMapButton);
+
+    expect(changeMapButton).toBeInTheDocument();
+    expect(mapOfHyrule).not.toBeInTheDocument();
+    expect(screen.getByAltText(mapLoruleAltText)).toBeInTheDocument();
+  });
+
+  test("Renders the correct Map after the change map button is toggled", () => {
+    render(<Map />);
+
+    const mapOfHyrule = screen.getByAltText(mapHyruleAltText);
+    const changeMapButton = screen.getByAltText(changeMapButtonAltText);
+
+    expect(mapOfHyrule).toBeInTheDocument();
+    expect(changeMapButton).toBeInTheDocument();
+
+    fireEvent.click(changeMapButton);
+
+    expect(changeMapButton).toBeInTheDocument();
+    expect(mapOfHyrule).not.toBeInTheDocument();
+
+    const mapOfLorule = screen.getByAltText(mapLoruleAltText);
+    expect(mapOfLorule).toBeInTheDocument();
+
+    fireEvent.click(changeMapButton);
+
+    expect(changeMapButton).toBeInTheDocument();
+    expect(mapOfLorule).not.toBeInTheDocument();
+
+    expect(mapOfHyrule).toBeInTheDocument();
+  });
+});
---
+++ src/ui/map.tsx
@@ -0,0 +1,14 @@
+import _ from "lodash";
+import React from "react";
+
+class Map extends React.Component {
+
+  render(): React.ReactNode {
+    return (
+      <div className="map">
+      </div>
+    );
+  }
+}
+
+export default Map;
